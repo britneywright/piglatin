9a245276235e2cfe767eed122a151c508d564254
Britney Wright <blw06g@gmail.com>
2014-01-03 13:04:39 -0500

    updated to account for words that start with vowels

--- test.rb
+++ test.rb
@@ -1,23 +1,26 @@
 require 'minitest/autorun'
 
 describe "english to pig-latin translation" do
-	it "should move the first letter of the word to the end and adds ay at the end" do
+	it "should move the first letter of the word to the end and add ay at the end to words that start with consonants" do
 		translate_word("pig").must_equal "igpay"
 	end
 
+	it "should add way to the end of words that start with vowels" do
+		translate_word("idle").must_equal "idleway"
+	end
+
 	it "should write each word of a phrase in pig-latin" do
-		translate_phrase("went to the park").must_equal "entway otay hetay arkpay"
+		translate_phrase("went to go away").must_equal "entway otay ogay awayway"
 	end
 
 	it "should write a sentence in pig-latin" do
 		translate_sentence("What do you want to do today?").must_equal "Hatway oday ouyay antway otay oday odaytay?"	
 	end
-
 end
 
 def translate_word(word)
-	word[1..-1] + word[0] + "ay"
-end
+	match = (first_letter = word[0].match(/[aeiou]/)) ? (word + "way") : (word[1..-1] + word[0] + "ay")
+end	
 
 def translate_phrase(phrase)
 	phrase.split(' ').map { |word| translate_word(word) }.join(' ')
@@ -26,3 +29,4 @@ end
 def translate_sentence(sentence)
 	sentence.downcase.gsub(/\w+/){|word| translate_word(word)}.capitalize
 end
+ 
